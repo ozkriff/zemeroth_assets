4a312db2bc86f050a73dd841ecb7aa18a64c01e6
Andrey Lesnikov <ozkriff@gmail.com>
2017-07-12 12:23:47 +0300

    init

---
+++ shader/v.glsl
@@ -0,0 +1,9 @@
+uniform mat4 u_ModelViewProj;
+attribute vec2 a_Pos;
+attribute vec2 a_Uv;
+varying vec2 v_Uv;
+
+void main() {
+    v_Uv = a_Uv;
+    gl_Position = u_ModelViewProj * vec4(a_Pos, 0.0, 1.0);
+}
